040583ce9288904263237b112c219962cb174fb5
Aaron Amankwaah <aaronamankwaah@nwk-81-242.nwk.ruw.rutgers.edu>
2020-12-17 09:58:26 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, Keyboard,  View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Platform,Button, Image, TouchableHighlight,} from 'react-native';
+import { StyleSheet, Text, Keyboard, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Platform, Button, Image, TouchableHighlight, } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import Info from './src/login/homescreen.js';
 import Setup from './src/login/signup.js';
@@ -12,33 +12,30 @@ import FamilyListSuggested from './src/lists/famListSuggested.js';
 import FriendList from './src/lists/friendList.js';
 import FriendListSuggested from './src/lists/friendListSuggested.js';
 import Gallery from './src/images/gallery.js';
-import List from './src/lists/userLists.js'; 
- 
-const RootStack = createStackNavigator(
-{
-  Info: { screen: Info },
-  Setup: { screen: Setup },
-  Register: { screen: Register },
-  UserLists: { screen: UserLists },
-  PersonalList: { screen: PersonalList },
-  PersonalListSuggested: { screen: PersonalListSuggested },
-  FamilyList: { screen: FamilyList },
-  FamilyListSuggested: { screen: FamilyListSuggested },
-  FriendList: { screen: FriendList },
-  FriendListSuggested: { screen: FriendListSuggested },
-  Gallery: { screen: Gallery },
-  UserList: { screen: List },
- 
-},
-{
-  initialRouteName: 'Info',
-}
+import List from './src/lists/userLists.js';
+
+const RootStack = createStackNavigator({
+        Info: { screen: Info },
+        Setup: { screen: Setup },
+        Register: { screen: Register },
+        UserLists: { screen: UserLists },
+        PersonalList: { screen: PersonalList },
+        PersonalListSuggested: { screen: PersonalListSuggested },
+        FamilyList: { screen: FamilyList },
+        FamilyListSuggested: { screen: FamilyListSuggested },
+        FriendList: { screen: FriendList },
+        FriendListSuggested: { screen: FriendListSuggested },
+        Gallery: { screen: Gallery },
+        UserList: { screen: List },
+
+    }, {
+        initialRouteName: 'Info',
+    }
 
 );
 
 export default class App extends Component {
-  render() {
-    return <RootStack />;
-  }
+    render() {
+        return <RootStack / > ;
+    }
 }
-
